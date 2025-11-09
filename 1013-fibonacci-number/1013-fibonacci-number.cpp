ac12eb3f805f434c9572c05f4153cb055296a67e
Harshita Gupta <91972448+Harshita-gupta10@users.noreply.github.com>
2025-11-10 01:09:08 +0530

    Time: 3 ms (65.01%), Space: 8.1 MB (16.82%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
-    int fibo(int n, vector<int>& dp)
-    {
-        // base case
-        if(n == 0 || n == 1)
-        {
-            return n;
-        }
-        // step 3
-        if(dp[n] != -1)
-        {
-            return dp[n];
-        }
-        // step 2
-        dp[n] = (fibo(n-1, dp)+fibo(n-2, dp));
-        return dp[n];
-    }
     int fib(int n) {
         // step 1
         vector<int> dp(n+1);
-        for(int i = 0; i < n+1; i++)
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        // base case
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++)
         {
-            dp[i] = -1;
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        return fibo(n, dp);
+        return dp[n];
     }
 };
